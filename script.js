48f5cd591fbb1b379ec821a77bcfad1e867adc4e
melcave43 <melcave77@gmail.com>
2022-06-15 22:45:28 -0400

    Update JS weather icon

--- script.js
+++ script.js
@@ -34,6 +34,9 @@ function displayWeatherCondition(response) {
   );
   document.querySelector("#description").innerHTML =
     response.data.weather[0].main;
+
+    let iconElement = document.querySelector("#icon");
+iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
 }
 
 
